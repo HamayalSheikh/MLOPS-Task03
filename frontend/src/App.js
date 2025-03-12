39c28ff3583b61cac361a4601b60ee97104f95e2
HamayalSheikh <hamyl.sheikh90@gmail.com>
2025-03-12 16:43:28 +0500

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Frontend Connected to Backend</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
